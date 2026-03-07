c4e03366e19bbdee90b59bfd0303dc10b75f5eed
Gaurav Kumar <gauravraaz2005@gmail.com>
2026-03-07 14:46:20 +0530

    add mbile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 import { handleSocketEvents } from './sockets/safetySocket.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = http.createServer(app);
 
 // Socket.io initialization
